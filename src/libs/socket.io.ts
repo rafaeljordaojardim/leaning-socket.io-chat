571cb5647e63ce074ba4dd66f23072101c2658a9
rafaeljordaojardim <you@example.com>
2020-01-19 15:22:58 -0300

    adding some improviments

---
+++ src/libs/socket.io.ts
@@ -0,0 +1,25 @@
+
+export default (io) => {
+  io.on("connection", (socket) => {
+    socket.username = "Anonymous";
+
+    socket.broadcast.emit("userConnected", { message: `user ${socket.username} connected` });
+
+    socket.on("change_user", (user) => {
+      socket.username = user.name;
+      console.log(socket.username);
+    });
+
+    socket.on("send_message", (data) => {
+      io.emit("new_message", {
+        message: `${socket.username}: ${data.message}`
+      });
+    });
+
+    socket.on('disconnect', (reason) => {
+      socket.broadcast.emit("disconnected", {
+        message: `${socket.username} was disconnected`
+      })
+    });
+  });
+}
